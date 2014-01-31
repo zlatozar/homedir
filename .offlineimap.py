4a69bc3e9943a1210364aad4af500608a3a09198
Zlatozar Zhelyazkov <zzhelyaz@cisco.com>
2014-01-31 16:36:27 +0200

    Create projects structure
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- .offlineimap.py
+++ .offlineimap.py
@@ -3,6 +3,7 @@
 import re, os
 
 # ~/.authinfo is encrypted using emacs: M-x epa-encrypt-file
+# Do not froget to remove ~/.authinfo after that
 def get_password_emacs(machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
