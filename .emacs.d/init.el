32e5133d2c4041239d35a78e326342f8998c423d
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2014-03-23 12:29:12 +0200

    Add ielm auto-complete
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -887,6 +887,18 @@ plus add font-size: 10pt"
 ;; Emacs Lisp REPL
 (add-hook 'ielm-mode-hook 'my/emacs-lisp-setup)
 
+(defun ielm-auto-complete ()
+  "Enables `auto-complete' support in \\[ielm]."
+  (setq ac-sources '(ac-source-functions
+                     ac-source-variables
+                     ac-source-features
+                     ac-source-symbols
+                     ac-source-words-in-same-mode-buffers))
+  (add-to-list 'ac-modes 'inferior-emacs-lisp-mode)
+  (auto-complete-mode 1))
+
+(add-hook 'ielm-mode-hook 'ielm-auto-complete)
+
 (defun visit-ielm ()
   (interactive)
   (if (not (get-buffer "*ielm*"))
