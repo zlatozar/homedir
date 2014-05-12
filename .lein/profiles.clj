36aa49c4f3c2112c02bea127c5106b8fb281e686
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2014-05-12 11:26:42 +0300

    Configure 0.7 cider-nrepl
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,5 @@
 {:user
  ;; https://github.com/technomancy/leiningen/wiki/Plugins
  {:plugins [[lein-ancient "0.5.4"]
-            [lein-ritz "0.7.0"]]
-  :dependencies [[nrepl-inspect "0.3.0"]
-                 [clojure-complete "0.2.3"]
-                 [ritz/ritz-nrepl-middleware "0.7.0"]]
-  :repl-options {:nrepl-middleware
-                 [inspector.middleware/wrap-inspect
-                  ritz.nrepl.middleware.javadoc/wrap-javadoc
-                  ritz.nrepl.middleware.apropos/wrap-apropos]}}}
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+  :dependencies [[clojure-complete "0.2.3"]]}}
