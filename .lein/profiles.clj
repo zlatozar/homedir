464d45173b8493a8a89f248a457090e5bd209f92
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2014-06-30 18:09:03 +0300

    Add Clojure tools
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,8 @@
 {:user
  ;; https://github.com/technomancy/leiningen/wiki/Plugins
- {:plugins [[lein-ancient "0.5.4"]
+ {:plugins [[lein-ancient "0.5.5"]
             [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
-  :dependencies [[clojure-complete "0.2.3"]]}}
+
+  :dependencies [[clojure-complete "0.2.3"]
+                 [org.clojure/tools.trace "0.7.8"]
+                 [instaparse "1.3.2"]]}}
