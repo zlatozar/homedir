233dad3d8b7c152b2076b0d6ec2040df396926f2
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2014-07-17 18:43:54 +0300

    C-x C-i Ispell and Abbrev, the Perfect Auto-Correct
    Source: http://endlessparentheses.com/ispell-and-abbrev-the-perfect-auto-correct.html
    
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -610,6 +610,25 @@ plus add font-size: 10pt"
 ;;_______________________________________________________________________________
 ;;                                                                      Spelling
 
+(define-key ctl-x-map "\C-i" 'endless/ispell-word-then-abbrev)
+
+(defun endless/ispell-word-then-abbrev (p)
+  "Call `ispell-word'. Then create an abbrev for the correction made.
+With prefix P, create local abbrev. Otherwise it will be global."
+  (interactive "P")
+  (let ((bef (downcase (or (thing-at-point 'word) ""))) aft)
+    (call-interactively 'ispell-word)
+    (setq aft (downcase (or (thing-at-point 'word) "")))
+    (unless (string= aft bef)
+      (message "\"%s\" now expands to \"%s\" %sally"
+               bef aft (if p "loc" "glob"))
+      (define-abbrev
+        (if p global-abbrev-table local-abbrev-table)
+        bef aft))))
+
+(setq save-abbrevs t)
+(setq-default abbrev-mode t)
+
 (setq ispell-program-name "aspell"
       ispell-extra-args '("--sug-mode=normal"))
 
