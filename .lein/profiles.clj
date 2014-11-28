18497d254277eef1f9d2fc5c314c492a222791ff
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2014-11-28 12:19:20 +0200

    Add notes
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,9 @@
+;; Do not forget to check versions
+
 {:user
  ;; https://github.com/technomancy/leiningen/wiki/Plugins
  {:plugins [[lein-ancient "0.5.5"]
-            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]] ; <-- Should be compatible with CIDER version
 
   :dependencies [[clojure-complete "0.2.3"]
                  [org.clojure/tools.trace "0.7.8"]
